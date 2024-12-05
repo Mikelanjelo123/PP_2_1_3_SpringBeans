3d3f1b102dc0fab20336c833b350fde8ffed18c7
Mikelanjelo <m.13021999@mail.ru>
2024-12-05 15:34:44 +0300

    ready

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,30 +1,9 @@
 package app.config;
 
-import app.model.Cat;
-import app.model.Dog;
-import app.model.Timer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
-    @Bean(name = "cat")
-    public Cat cat() {
-        Cat cat = new Cat();
-        cat.toString();
-        return cat;
-    }
-    @Bean(name = "dog")
-    public Dog dog() {
-        Dog dog = new Dog();
-        dog.toString();
-        return dog;
-    }
-    @Bean(name = "timer")
-    public Timer timer() {
-        Timer timer = new Timer();
-        return timer;
-    }
 }
--- src/main/java/app/model/Cat.java
+++ src/main/java/app/model/Cat.java
@@ -1,8 +1,10 @@
 package app.model;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(scopeName = "prototype")
 public class Cat extends Animal {
     @Override
     public String toString() {
