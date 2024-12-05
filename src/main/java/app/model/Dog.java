2737d3a5f58e6f4ddbbd3f9b5e717326a4251b7b
Mikelanjelo <m.13021999@mail.ru>
2024-12-05 15:42:35 +0300

    ready

--- src/main/java/app/model/Dog.java
+++ src/main/java/app/model/Dog.java
@@ -1,10 +1,8 @@
 package app.model;
 
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope(scopeName = "prototype")
 public class Dog extends Animal {
     @Override
     public String toString() {
